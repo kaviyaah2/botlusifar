dd2dc3285c377df1c1582f2bfd2d2edf3883b8c7
kaviyaah2 <97598994+kaviyaah2@users.noreply.github.com>
2022-01-17 06:53:12 +0530

    V 8.0.0 AVALABLE NOW🎠

---
+++ helpers.js
@@ -0,0 +1,21 @@
+/* Copyright (C) 2021 KAVIYAAH - Alexa Team  ,  Lusifar whatsapp bot owner
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+kaviyaah - kavishka sandaruwan (v 8.0.0 avalable)
+*/
+function successfullMessage(msg) {
+    return "✅ *Lusifar*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🛑 *Lusifar*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "⏺️ *Lusifar*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
